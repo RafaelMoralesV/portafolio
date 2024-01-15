847d2852d367f136efc9b15c1b96ff3fa6c50a6e
Rafael Morales <rmorales@utem.cl>
2024-01-14 23:39:42 -0300

    Experimentando con themes

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -6,8 +6,21 @@ module.exports = {
 	},
 	plugins: [require("@tailwindcss/typography"), require("daisyui")],
 	daisyui: {
-		themes: true, // true: all themes | false: only light + dark | array: specific themes like this ["light", "dark", "cupcake"]
-		darkTheme: "dark", // name of one of the included themes for dark mode
+		themes: [
+			{
+				"catppuccin-mocha": {
+					primary: "#89b4fa", // blue
+					secondary: "#f5c2e7", // pink
+					accent: "#94e2d5", // teal
+					neutral: "#11111b", // crust
+					"base-100": "#1e1e2e", // base
+					info: "#74c7ec", // sapphire
+					success: "#a6e3a1", // green
+					warning: "#f9e2af", // yellow
+					error: "#f38ba8", // red
+				},
+			}, // true: all themes | false: only light + dark | array: specific themes like this ["light", "dark", "cupcake"]
+		],
 		logs: false, // Shows info about daisyUI version and used config in the console when building your CSS
 	},
 };
